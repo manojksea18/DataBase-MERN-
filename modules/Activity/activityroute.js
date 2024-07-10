e922dda3e5eccf171284da7a2ad13b584f8db533
Manoj Khati <mksea18@gmail.com>
2024-07-10 21:53:22 +0545

    updated

--- modules/Activity/activityroute.js
+++ modules/Activity/activityroute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const activityRoutes = require("./activitycontroller");
+const Contoller = require("./activitycontroller");
 
 router.get("/", async (req, res, next) => {
   try {
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 const activityRouter = require("../modules/Activity/activityroute");
 const subActivityRouter = require("../modules/subActivity/subactivityroute");
 
-router.use("api/v1/activities", activityRouter);
-router.use("api/v1/sub-activities", subActivityRouter);
+router.use("/api/v1/activities", activityRouter);
+router.use("/api/v1/sub-activities", subActivityRouter);
 
 module.exports = router;
